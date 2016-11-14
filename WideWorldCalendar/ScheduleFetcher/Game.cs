79ef6461c157c45511d94f9363f6dace93f03e1d
Matt Hoffman <hofmadresu@gmail.com>
2016-11-14 18:06:42 -0500

    Format display better

--- WideWorldCalendar/ScheduleFetcher/Game.cs
+++ WideWorldCalendar/ScheduleFetcher/Game.cs
@@ -4,7 +4,6 @@ namespace WideWorldCalendar.ScheduleFetcher
 	public class Game
 	{
 		public DateTime ScheduledDateTime { get; set; }
-		public string GameTimeString => ScheduledDateTime.ToString("d") + " " + ScheduledDateTime.ToString("t");
 		public string Field { get; set; }
 		public bool IsHomeGame { get; set; }
 		public Team MyTeam { get; set; }
