9158650786954054ddfdb415ea99bf334c75c39b
Matt Hoffman <hofmadresu@gmail.com>
2016-11-17 14:13:41 -0500

    Colorize rows if game is past

---
+++ WideWorldCalendar/Converters/GameDateFutureColorConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace WideWorldCalendar.Converters
+{
+    public class GameDateFutureColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DateTime.Now < ((DateTime)value).AddHours(1) ? Color.White : Color.FromHex("#cccccc");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
