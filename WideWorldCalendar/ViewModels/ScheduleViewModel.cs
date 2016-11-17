fffd67207821cf3430ddfbb215f133d8a41ab725
Matt Hoffman <hofmadresu@gmail.com>
2016-11-17 10:27:05 -0500

    Add save button and empty command

--- WideWorldCalendar/ViewModels/ScheduleViewModel.cs
+++ WideWorldCalendar/ViewModels/ScheduleViewModel.cs
@@ -1,10 +1,24 @@
-﻿using MvvmHelpers;
+﻿using System.Windows.Input;
+using MvvmHelpers;
 using WideWorldCalendar.ScheduleFetcher;
+using Xamarin.Forms;
 
 namespace WideWorldCalendar.ViewModels
 {
 	public class ScheduleViewModel : BaseViewModel
 	{
+        public ScheduleViewModel()
+	    {
+	        SaveCommand = new Command(_ =>
+	        {
+	            
+	        });
+        }
+
+
 	    public ObservableRangeCollection<Game> Games { get; } = new ObservableRangeCollection<Game>();
-	}
+
+        public ICommand SaveCommand { protected set; get; }
+
+    }
 }
