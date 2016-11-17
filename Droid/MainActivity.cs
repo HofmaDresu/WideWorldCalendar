b731f3243b7d1926703aaf2e9c8c1a01bfc1858d
Matt Hoffman <hofmadresu@gmail.com>
2016-11-17 14:16:06 -0500

    Update App Name

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,16 +1,10 @@
-﻿using System;
-
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace WideWorldCalendar.Droid
 {
-	[Activity(Label = "WideWorldCalendar.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "Wide World Calendar", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle bundle)
