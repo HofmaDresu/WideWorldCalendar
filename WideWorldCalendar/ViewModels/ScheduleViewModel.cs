90b66580a8a1ae6886776dc994fbaaecd11be390
Matt Hoffman <hofmadresu@gmail.com>
2016-11-17 21:15:10 -0500

    Handle double headers + refactor

--- WideWorldCalendar/ViewModels/ScheduleViewModel.cs
+++ WideWorldCalendar/ViewModels/ScheduleViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Input;
 using MvvmHelpers;
@@ -14,58 +15,91 @@ namespace WideWorldCalendar.ViewModels
 
         public ScheduleViewModel(INavigation navigation)
         {
+            var gameDays = new Dictionary<DateTime, List<Persistence.Models.Game>>();
             _navigation = navigation;
             SaveCommand = new Command(_ =>
 	        {
 	            var data = Data.GetInstance();
                 var localNotification = DependencyService.Get<ILocalNotification>();
 
-                var myTeamInfo = Games.First().MyTeam;
-	            var myTeam = new Persistence.Models.MyTeam
-	            {
-                    TeamName = myTeamInfo.Name,
-                    TeamColor = myTeamInfo.Color,
-                    Division = myTeamInfo.Division,
-                    LastGameDateTime = Games.OrderBy(g => g.ScheduledDateTime).Last().ScheduledDateTime
-	            };
-
-	            data.InsertMyTeam(myTeam);
+                var myTeam = SaveMyTeam(data);
 
 	            foreach (var gameInfo in Games)
 	            {
-	                var game = new Persistence.Models.Game
-	                {
-	                    Field = gameInfo.Field,
-                        IsHomeGame = gameInfo.IsHomeGame,
-                        MyTeamId = myTeam.Id,
-                        ScheduledDateTime = gameInfo.ScheduledDateTime,
-                        OpposingTeam = new Persistence.Models.OpposingTeam
-                        {
-                            TeamName = gameInfo.OpposingTeam.Name,
-                            TeamColor = gameInfo.OpposingTeam.Color
-                        }
-	                };
-	                data.InsertGame(game);
+	                var game = SaveGame(gameInfo, myTeam, data);
 
-                    //TODO: Handle double headers
-                    //TODO: ask if user wants notifications
-	                var notificationTitle = "Soccer Tonight!";
-	                var notificationMessage = $"{myTeam.TeamName} @ {game.ScheduledDateTime.ToString("t")}";
-	                if (game.ScheduledDateTime.Date > DateTime.Now.Date)
+	                if (gameDays.ContainsKey(game.ScheduledDateTime.Date))
 	                {
-                        localNotification.ScheduleGameNotification(notificationTitle, notificationMessage, game.Id, game.ScheduledDateTime.Date.AddHours(9));
-                    }
-#if DEBUG
-                    if (game.ScheduledDateTime.Date == DateTime.Now.Date)
-                    {
-                        localNotification.ScheduleGameNotification(notificationTitle, notificationMessage, game.Id, DateTime.Now.AddMinutes(1));
-                    }
-#endif
+	                    gameDays[game.ScheduledDateTime.Date].Add(game);
+	                }
+	                else
+	                {
+	                    gameDays.Add(game.ScheduledDateTime.Date, new List<Persistence.Models.Game> { game });
+	                }
+	            }
+
+	            //TODO: ask if user wants notifications
+                foreach (var day in gameDays)
+                {
+                    CreateNotification(myTeam, day.Value, localNotification);
                 }
 
 	            _navigation.PopToRootAsync(true);
 	        });
         }
+
+	    private static Persistence.Models.Game SaveGame(Game gameInfo, Persistence.Models.MyTeam myTeam, Data data)
+	    {
+	        var game = new Persistence.Models.Game
+	        {
+	            Field = gameInfo.Field,
+	            IsHomeGame = gameInfo.IsHomeGame,
+	            MyTeamId = myTeam.Id,
+	            ScheduledDateTime = gameInfo.ScheduledDateTime,
+	            OpposingTeam = new Persistence.Models.OpposingTeam
+	            {
+	                TeamName = gameInfo.OpposingTeam.Name,
+	                TeamColor = gameInfo.OpposingTeam.Color
+	            }
+	        };
+	        data.InsertGame(game);
+	        return game;
+	    }
+
+	    private Persistence.Models.MyTeam SaveMyTeam(Data data)
+	    {
+	        var myTeamInfo = Games.First().MyTeam;
+	        var myTeam = new Persistence.Models.MyTeam
+	        {
+	            TeamName = myTeamInfo.Name,
+	            TeamColor = myTeamInfo.Color,
+	            Division = myTeamInfo.Division,
+	            LastGameDateTime = Games.OrderBy(g => g.ScheduledDateTime).Last().ScheduledDateTime
+	        };
+
+	        data.InsertMyTeam(myTeam);
+	        return myTeam;
+	    }
+
+	    private static void CreateNotification(Persistence.Models.MyTeam myTeam, List<Persistence.Models.Game> games, ILocalNotification localNotification)
+	    {
+	        if (!games.Any()) return;
+
+	        var notificationTitle = games.Count == 1 ? "Game Tonight!" : "Double Header Tonight!";
+	        var notificationMessage = $"{myTeam.TeamName} @ {string.Join(",", games.Select(g => g.ScheduledDateTime.ToString("t")))}";
+	        if (games.First().ScheduledDateTime.Date > DateTime.Now.Date)
+	        {
+	            localNotification.ScheduleGameNotification(notificationTitle, notificationMessage, games.First().Id,
+                    games.First().ScheduledDateTime.Date.AddHours(9));
+	        }
+#if DEBUG
+	        if (games.First().ScheduledDateTime.Date == DateTime.Now.Date)
+	        {
+	            localNotification.ScheduleGameNotification(notificationTitle, notificationMessage, games.First().Id,
+	                DateTime.Now.AddMinutes(1));
+	        }
+#endif
+	    }
 
 
 	    public ObservableRangeCollection<Game> Games { get; } = new ObservableRangeCollection<Game>();
