2212de7a5827166045b552794a706e8cb0c3ceef
Matt Hoffman <hofmadresu@gmail.com>
2016-11-22 11:15:26 -0500

    Add class for iOS notifications

---
+++ iOS/LocalNotification_iOS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WideWorldCalendar.iOS;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(LocalNotification_iOS))]
+namespace WideWorldCalendar.iOS
+{
+    public class LocalNotification_iOS : ILocalNotification
+    {
+        public void ScheduleGameNotification()
+        {
+        }
+    }
+}
