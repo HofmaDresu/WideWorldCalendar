1a0006705ce582a9e3235a1dcc0ab6c7d6ce73cf
Matt Hoffman <hofmadresu@gmail.com>
2016-11-28 15:54:30 -0500

    Add secondary color to core. Apply button them to SelecteSchedulePage

--- WideWorldCalendar/Colors.cs
+++ WideWorldCalendar/Colors.cs
@@ -7,6 +7,8 @@ namespace WideWorldCalendar
         public static Color PrimaryColor = Color.FromHex("#1939ff");
         public static Color PrimaryHightlightColor = Color.FromHex("#CC1939ff");
         public static Color PrimaryDisabledColor = Color.FromHex("#331939ff");
-        public static Color SecondaryColor = Color.FromHex("#1939ff");
+        public static Color SecondaryColor = Color.FromHex("#ffc300");
+        public static Color SecondaryHightlightColor = Color.FromHex("#ccffc300");
+        public static Color SecondaryDisabledColor = Color.FromHex("#eeffc300");
     }
 }
