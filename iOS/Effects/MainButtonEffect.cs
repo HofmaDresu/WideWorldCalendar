48637143b52350011c3f788912c19f15734ce83f
Matt Hoffman <hofmadresu@gmail.com>
2016-11-28 16:09:39 -0500

    Initial code for iOS main button effect

---
+++ iOS/Effects/MainButtonEffect.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using UIKit;
+using WideWorldCalendar.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName(EffectIds.EffectGroupName)]
+[assembly: ExportEffect(typeof(MainButtonEffect), EffectIds.MainButtonEffectName)]
+namespace WideWorldCalendar.iOS.Effects
+{
+    public class MainButtonEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                var uiButton = Control as UIButton;
+                if (uiButton == null) return;
+
+                Control.BackgroundColor = uiButton.Enabled ? Colors.PrimaryColor.ToUIColor() : Colors.PrimaryDisabledColor.ToUIColor();
+                Control.Layer.CornerRadius = 4.0f;
+            }
+            catch (Exception)
+            {
+                //IGNORE
+            }
+        }
+
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            var uiButton = Control as UIButton;
+            if (uiButton == null) return;
+
+            try
+            {
+                if (args.PropertyName == "IsFocused")
+                {
+                    Control.BackgroundColor = Equals(Control.BackgroundColor, Colors.PrimaryColor.ToUIColor()) ? Colors.PrimaryHightlightColor.ToUIColor() : Colors.PrimaryColor.ToUIColor();
+                }
+                if (args.PropertyName == "IsEnabled")
+                {
+                    Control.BackgroundColor = uiButton.Enabled ? Colors.PrimaryDisabledColor.ToUIColor() : Colors.PrimaryColor.ToUIColor();
+                }
+            }
+            catch (Exception)
+            {
+                //IGNORE
+            }
+        }
+
+        protected override void OnDetached()
+        {
+            //Not Needed
+        }
+    }
+}
