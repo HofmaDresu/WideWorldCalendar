56b7b97bf1c20926780974c88a0f20c31dba8844
Matt Hoffman <hofmadresu@gmail.com>
2016-11-29 11:04:13 -0500

    Implement analytics on iOS

---
+++ iOS/ThirdPartyIds.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WideWorldCalendar.iOS
+{
+    public static class ThirdPartyIds
+    {
+        public const string AnalyticsTrackingId = "UA-88212615-1";
+    }
+}
---
+++ iOS/Utilities/UnifiedAnalytics_iOS.cs
@@ -0,0 +1,60 @@
+﻿using Foundation;
+using Google.Analytics;
+using WideWorldCalendar.UtilityInterfaces;
+
+namespace WideWorldCalendar.iOS.Utilities
+{
+    public class UnifiedAnalytics_iOS : IUnifiedAnalytics
+    {
+        private UnifiedAnalytics_iOS()
+        {
+            Gai.SharedInstance.GetTracker(ThirdPartyIds.AnalyticsTrackingId);
+        }
+
+        private static UnifiedAnalytics_iOS _unifiedAnalytics;
+        public static UnifiedAnalytics_iOS GetInstance()
+        {
+            return _unifiedAnalytics ?? (_unifiedAnalytics = new UnifiedAnalytics_iOS());
+        }
+
+        private static ITracker DefaultTracker
+        {
+            get
+            {
+#if DEBUG
+                Gai.SharedInstance.DryRun = true;
+#endif
+
+                Gai.SharedInstance.DefaultTracker.SetAllowIdfaCollection(true);
+
+                return Gai.SharedInstance.DefaultTracker;
+            }
+        }
+
+        public void SendScreenHitOnDefaultTracker(string screenName)
+        {
+            Gai.SharedInstance.DefaultTracker.Set(GaiConstants.ScreenName, screenName);
+
+            Gai.SharedInstance.DefaultTracker.Send(DictionaryBuilder.CreateScreenView().Build());
+        }
+
+        public void SetOnDefaultTracker(string key, string value)
+        {
+            DefaultTracker.Set(key, value);
+        }
+        private static void SendOnDefaultTracker(NSDictionary eventToSend)
+        {
+            DefaultTracker.Send(eventToSend);
+        }
+
+        public void CreateAndSendEventOnDefaultTracker(string category, string action, string label, long? value)
+        {
+            SendOnDefaultTracker(CreateEvent(category, action, label, value));
+        }
+
+        private static NSDictionary CreateEvent(string category, string action, string label, long? value)
+        {
+            return DictionaryBuilder.CreateEvent(category, action, label, value).Build();
+        }
+    }
+}
