bdb0f7a97a9440ad8c319de483947a5dd7b6f742
Matt Hoffman <hofmadresu@gmail.com>
2017-01-06 17:08:20 -0500

    Use different templates for scored and unscored games

--- WideWorldCalendar/ViewModels/TeamScheduleViewModel.cs
+++ WideWorldCalendar/ViewModels/TeamScheduleViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MvvmHelpers;
+﻿using MvvmHelpers;
 using WideWorldCalendar.Persistence;
 using WideWorldCalendar.Persistence.Models;
 
