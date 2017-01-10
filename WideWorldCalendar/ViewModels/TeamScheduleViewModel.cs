eb67758c8bbe9a4d4d5b6525899747b30ff55bdd
Matt Hoffman <hofmadresu@gmail.com>
2017-01-10 12:01:02 -0500

    Add refresh command

--- WideWorldCalendar/ViewModels/TeamScheduleViewModel.cs
+++ WideWorldCalendar/ViewModels/TeamScheduleViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using MvvmHelpers;
+using System.Windows.Input;
 using WideWorldCalendar.Persistence;
 using WideWorldCalendar.Persistence.Models;
+using Xamarin.Forms;
 
 namespace WideWorldCalendar.ViewModels
 {
@@ -9,8 +11,15 @@ namespace WideWorldCalendar.ViewModels
         public TeamScheduleViewModel(int myTeamId)
         {
             Games.AddRange(Data.GetInstance().GetGames(myTeamId));
+
+            RefreshGamesCommand = new Command(_ =>
+            {
+
+            });
         }
 
         public ObservableRangeCollection<Game> Games { get; } = new ObservableRangeCollection<Game>();
+
+        public ICommand RefreshGamesCommand { get; set; }
     }
 }
