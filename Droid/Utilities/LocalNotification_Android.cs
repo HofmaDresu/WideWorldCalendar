872790e6dd2babd806727b904fee93e86eb52b7d
Matt Hoffman <hofmadresu@gmail.com>
2017-01-18 13:11:14 -0500

    Split apart notification types. Add url to Game Notifications

--- Droid/Utilities/LocalNotification_Android.cs
+++ Droid/Utilities/LocalNotification_Android.cs
@@ -45,7 +45,7 @@ namespace WideWorldCalendar.Droid.Utilities
             //No implementation needed on Android. No notifications are "scheduled", and this is fully controlled in the ScheduleCheckBroadcastReceiver
         }
         
-        public static void CreateNotification(Context context, int requestCode, string title, string message)
+        public static void CreateBasicNotification(Context context, int requestCode, string title, string message)
         {
             var builder = new NotificationCompat.Builder(context)
                 .SetContentTitle(title)
@@ -79,6 +79,47 @@ namespace WideWorldCalendar.Droid.Utilities
 
             var notificationManager = NotificationManagerCompat.From(context);
             notificationManager?.Notify(requestCode, notification);
+        }
+
+        public static void CreateGameNotification(Context context, int teamId, string title, string message)
+        {
+            var notificationIntent = new Intent(Intent.ActionView, Android.Net.Uri.Parse($"wwc://WideWorldCalendar/TeamSchedule?id={teamId}"));
+            var clickPendingIntent = PendingIntent.GetActivity(context, 0, notificationIntent, 0);
+
+            var builder = new NotificationCompat.Builder(context)
+                .SetContentTitle(title)
+                .SetContentText(message);
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                builder
+                    .SetPriority((int)NotificationPriority.Low)
+                    .SetVisibility(NotificationCompat.VisibilityPublic)
+                    .SetCategory("reminder")
+                    .SetSmallIcon(Resource.Drawable.ic_launcher);
+            }
+            else if ((int)Build.VERSION.SdkInt >= 20)
+            {
+                builder
+                    .SetSmallIcon(Resource.Drawable.ic_launcher);
+            }
+            else
+            {
+                // Disable obsolete warning 'cause this was how you do it pre-20
+#pragma warning disable 618
+                builder
+                    .SetSmallIcon(Resource.Drawable.ic_launcher);
+#pragma warning restore 618
+            }
+
+            var notification = builder
+                .SetContentIntent(clickPendingIntent)
+                .Build();
+
+            notification.Flags |= NotificationFlags.AutoCancel;
+
+            var notificationManager = NotificationManagerCompat.From(context);
+            notificationManager?.Notify(teamId, notification);
         }
     }
 }
