e9d79816b9de61f24b3d6668e890f5c7554d91db
Matt Hoffman <hofmadresu@gmail.com>
2017-04-14 10:56:38 -0400

    Adjust style names. Use static resources. Adjust row height. do button

--- WideWorldCalendar/Colors.cs
+++ WideWorldCalendar/Colors.cs
@@ -4,11 +4,11 @@ namespace WideWorldCalendar
 {
     public static class Colors
     {
-        public static Color PrimaryColor = Color.FromHex("#1939ff");
-        public static Color PrimaryHightlightColor = Color.FromHex("#CC1939ff");
-        public static Color PrimaryDisabledColor = Color.FromHex("#331939ff");
-        public static Color SecondaryColor = Color.FromHex("#ffc300");
-        public static Color SecondaryHightlightColor = Color.FromHex("#ccffc300");
-        public static Color SecondaryDisabledColor = Color.FromHex("#eeffc300");
+        public static Color PrimaryColor = Color.FromHex("#1686f3");
+        public static Color PrimaryHightlightColor = Color.FromHex("#CC1686f3");
+        public static Color PrimaryDisabledColor = Color.FromHex("#331686f3");
+        public static Color SecondaryColor = Color.FromHex("#061E48");
+        public static Color SecondaryHightlightColor = Color.FromHex("#cc061E48");
+        public static Color SecondaryDisabledColor = Color.FromHex("#ee061E48");
     }
 }
