5195feb4b4cbc4e189e9ac009570effb4355dfbb
Matt Hoffman <hofmadresu@gmail.com>
2017-06-12 13:28:43 -0400

    Add remaining bar type converters

---
+++ WideWorldCalendar/Converters/LastGameBarIsVisibleConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace WideWorldCalendar.Converters
+{
+    public class LastGameBarIsVisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null) return false;
+            var list = ((ListView)parameter).ItemsSource.Cast<object>().ToList();
+            var index = list.IndexOf(value);
+            return index == list.Count - 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
