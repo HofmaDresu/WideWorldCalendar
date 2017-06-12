b0f8e11eff53ee28891a15dff2debf4da499388b
Matt Hoffman <hofmadresu@gmail.com>
2017-06-12 14:32:48 -0400

    Extract xaml

---
+++ WideWorldCalendar/CustomControls/GameStatusBar.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WideWorldCalendar.CustomControls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GameStatusBar : StackLayout
+    {
+        public object GamesList { get; set; }
+
+        public GameStatusBar(object gamesList)
+            : base()
+        {
+            GamesList = gamesList;
+            InitializeComponent();
+        }
+    }
+}
