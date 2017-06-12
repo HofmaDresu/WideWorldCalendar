3d803c8e89f21579ae90e000a1e69c30f40e29ff
Matt Hoffman <hofmadresu@gmail.com>
2017-06-12 15:24:15 -0400

    Add round icons for android

--- Droid/SplashActivity.cs
+++ Droid/SplashActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace WideWorldCalendar.Droid
 {
-    [Activity(Theme = "@style/Theme.Splash", Icon = "@drawable/ic_launcher", MainLauncher = true, NoHistory = true)]
+    [Activity(Theme = "@style/Theme.Splash", Icon = "@drawable/ic_launcher", RoundIcon = "@drawable/ic_launcher_round", MainLauncher = true, NoHistory = true)]
     public class SplashActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
