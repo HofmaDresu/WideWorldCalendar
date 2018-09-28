24b9978e1bf0f0c9d3e6bd37ecc9660d9d7db9cf
Matt Hoffman <hofmadresu@gmail.com>
2018-09-28 11:29:46 -0400

    Begin updating for cookies

--- WideWorldCalendar.Core/ScheduleFetcher/RestScheduleFetcher.cs
+++ WideWorldCalendar.Core/ScheduleFetcher/RestScheduleFetcher.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -8,13 +9,34 @@ namespace WideWorldCalendar.ScheduleFetcher
 {
 	public class RestScheduleFetcher : IScheduleFetcher
     {
-        private const string BaseScheduleUrl = "https://apps.dashplatform.com/dash/index.php?Action=League%2Fstandings&_method=GET&noheader=&facilityID=1&programID=0";
+        private const string BaseAddress = "https://apps.dashplatform.com";
+        private const string BaseScheduleUrl = BaseAddress + "/dash/index.php?Action=League%2Fstandings&_method=GET&noheader=&facilityID=1&programID=0";
         private const string SeasonScheduleParameter = "SeasonID{0}";
-        private const string BaseTeamUrl = "https://apps.dashplatform.com/dash/index.php?Action=Team/index&company=wideworldsports&teamid={0}";
+        private const string BaseTeamUrl = BaseAddress + "/dash/index.php?Action=Team/index&company=wideworldsports&teamid={0}";
+
+        public RestScheduleFetcher()
+        {
+            handler = new HttpClientHandler
+            {
+                CookieContainer = cookieContainer
+            };
+            client = new HttpClient(handler) { Timeout = TimeSpan.FromSeconds(30) };
+        }
+
+        private readonly CookieContainer cookieContainer = new CookieContainer();
+        private readonly HttpClientHandler handler;
+        private readonly HttpClient client;
+        private CookieCollection cookies;
 
         public async Task<List<NavigationOption>> GetSeasons()
 		{
-			return ScheduleHtmlParser.GetSeasons(await GetClient().GetStringAsync(BaseScheduleUrl)).ToList();
+            await client.GetStringAsync(BaseScheduleUrl);
+            cookies = cookieContainer.GetCookies(new Uri(BaseScheduleUrl));
+
+            var seasonsHtml = await client.GetStringAsync(BaseScheduleUrl);
+
+
+            return ScheduleHtmlParser.GetSeasons(seasonsHtml).ToList();
         }
 
 		public async Task<Dictionary<string, List<NavigationOption>>> GetLeagues(int seasonId)
@@ -24,13 +46,8 @@ namespace WideWorldCalendar.ScheduleFetcher
 
 		public async Task<List<Game>> GetTeamSchedule(int teamId)
 		{
-			var divisionReportHtml = await GetClient().GetStringAsync(string.Format(BaseTeamUrl, teamId));
+			var divisionReportHtml = await client.GetStringAsync(string.Format(BaseTeamUrl, teamId));
 			return ScheduleHtmlParser.GetTeamSchedule(teamId, divisionReportHtml).ToList();
 		}
-
-	    private HttpClient GetClient()
-	    {
-	        return new HttpClient { Timeout = TimeSpan.FromSeconds(30) };
-	    }
 	}
 }
