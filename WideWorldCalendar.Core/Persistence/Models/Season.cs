11d50cf6fef7b828eee5e154e52dab978d00726b
Matt Hoffman <hofmadresu@gmail.com>
2018-09-28 11:29:08 -0400

    Add equality checks to Season

--- WideWorldCalendar.Core/Persistence/Models/Season.cs
+++ WideWorldCalendar.Core/Persistence/Models/Season.cs
@@ -4,10 +4,29 @@ using SQLite;
 namespace WideWorldCalendar.Persistence.Models
 {
     [Table("Seasons")]
-    public class Season
+    public class Season : IEquatable<Season>
     {
         [PrimaryKey]
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public static bool operator ==(Season left, Season right) => Equals(left, right);
+
+        public static bool operator !=(Season left, Season right) => !Equals(left, right);
+
+        public bool Equals(Season other)
+        {
+            return (Id, Name) == (other.Id, other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (obj is Season season) && Equals(season);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Id, Name).GetHashCode();
+        }
     }
 }
