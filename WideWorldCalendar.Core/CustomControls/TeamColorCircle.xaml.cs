a80ce5c0123fdb67752281c6320e2271f8bc8d09
Matt Hoffman <hofmadresu@gmail.com>
2019-03-24 11:39:57 -0400

    Split team color swatch into separate control

---
+++ WideWorldCalendar.Core/CustomControls/TeamColorCircle.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WideWorldCalendar.CustomControls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TeamColorCircle : ContentView
+    {
+        public TeamColorCircle()
+        {
+            InitializeComponent();
+        }
+        public Color Color
+        {
+            get => (Color)GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+
+        public static readonly BindableProperty ColorProperty =
+            BindableProperty.Create("Color", typeof(Color), typeof(TeamColorCircle), Color.Transparent);
+    }
+}
