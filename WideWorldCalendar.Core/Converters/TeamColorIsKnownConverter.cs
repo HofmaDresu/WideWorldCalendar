f69e211f1eccf7b992d4a65fa2c97888aac41262
Matt Hoffman <hofmadresu@gmail.com>
2019-03-24 12:45:20 -0400

    Don't show color circle if color is unknown

---
+++ WideWorldCalendar.Core/Converters/TeamColorIsKnownConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace WideWorldCalendar.Converters
+{
+    public class TeamColorIsKnownConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var xamColor = value as Color?;
+            var systemColor = value as System.Drawing.Color?;
+            return (xamColor.HasValue && xamColor.Value.A != 0)
+                || (systemColor.HasValue && systemColor.Value.A != 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
